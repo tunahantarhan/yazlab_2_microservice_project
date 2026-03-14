22fe35719cc95c96b29a6e0b69f60ba5b747c284
tunahantarhan <tunahantarhan99@gmail.com>
2026-03-14 15:47:03 +0300

    feat: user_service için asenkron motor veritabanı bağlantısı kuruldu.

---
+++ user_service/database.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+# user_db, izolasyon sağlanması ve veritabanı çakışması olmaması adına 27018 portunda.
+MONGO_URL = "mongodb://localhost:27018"
+
+client = AsyncIOMotorClient(MONGO_URL)
+database = client.user_db
+user_collection = database.get_collection("users")
