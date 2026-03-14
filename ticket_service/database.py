94440a60fd87e3b95cc2834f47ec4adf0fb3480b
tunahantarhan <tunahantarhan99@gmail.com>
2026-03-14 15:44:36 +0300

    feat: ticket_service için asenkron motor veritabanı bağlantısı kuruldu.

---
+++ ticket_service/database.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+# ticket db, docker-compose'da ayarladığımız standart port üzerinde.
+MONGO_URL = "mongodb://localhost:27017"
+
+client = AsyncIOMotorClient(MONGO_URL)
+database = client.ticket_db
+ticket_collection = database.get_collection("tickets")
