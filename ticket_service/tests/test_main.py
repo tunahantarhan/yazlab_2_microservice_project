82dbe4c067d315a290f7d636c343da4a52855e2f
tunahantarhan <tunahantarhan99@gmail.com>
2026-03-23 15:43:54 +0300

    chore(ticket): ticket service test altyapısı kuruldu ve health check testi eklendi.

---
+++ ticket_service/tests/test_main.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app) # fastapi test client'ı.
+
+# main.py'deki health check rotasına ulaşım
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "Ticket Service sağlıklı durumda."}
