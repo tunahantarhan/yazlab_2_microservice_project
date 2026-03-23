95e330bc2966b973e90398d4a4c5aa2bf6ba58c3
tunahantarhan <tunahantarhan99@gmail.com>
2026-03-23 15:58:42 +0300

    test(ticket): ticket service'deki GET /tickets rotası için veritabanı taklitli testler eklendi.

--- ticket_service/tests/test_main.py
+++ ticket_service/tests/test_main.py
@@ -5,6 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from fastapi.testclient import TestClient
 from main import app
+from unittest.mock import patch, AsyncMock
 
 client = TestClient(app) # fastapi test client'ı.
 
@@ -13,3 +14,38 @@ def test_health_check():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "Ticket Service sağlıklı durumda."}
+    
+# mock veritabanı ve veriler
+mock_db_data = [
+    {"id": 1, "event_name": "Fenerbahçe - Galatasaray Derbisi", "price": 2000.0, "available": True},
+    {"id": 2, "event_name": "Şampiyonlar Ligi Finali", "price": 5000.0, "available": False}
+]
+
+# "patch" ile main'deki ticket_collection.find'ı taklit ediyoruz
+@patch("main.ticket_collection.find")
+def test_list_all_tickets(mock_find):
+    mock_cursor = AsyncMock()
+    mock_cursor.to_list.return_value = mock_db_data
+    mock_find.return_value = mock_cursor
+
+    # test isteği
+    response = client.get("/tickets")
+
+    assert response.status_code == 200
+    assert len(response.json()) == 2
+    assert response.json()[0]["event_name"] == "Fenerbahçe - Galatasaray Derbisi"
+
+@patch("main.ticket_collection.find")
+def test_list_available_tickets(mock_find):
+    mock_cursor = AsyncMock()
+    mock_cursor.to_list.return_value = [mock_db_data[0]]
+    mock_find.return_value = mock_cursor
+
+    # filtreli test isteği
+    response = client.get("/tickets?available=true")
+
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+    assert response.json()[0]["available"] is True
+    
+    mock_find.assert_called_with({"available": True}, {"_id": 0})
