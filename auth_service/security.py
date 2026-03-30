2ffb247749dbecc6fdd0e42544e2142474fe09fb
tunahantarhan <tunahantarhan99@gmail.com>
2026-03-30 21:46:13 +0300

    feat(auth): gerçek jwt üretme ve doğrulama fonksiyonları eklendi.

--- auth_service/security.py
+++ auth_service/security.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
 import jwt
 
@@ -9,9 +9,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # token oluşturma (jwt) ayarları
 SECRET_KEY = os.getenv("JWT_SECRET_KEY", "gizli-auth-anahtari")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30  # token yarım saat geçerli olacak
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # token yarım saat geçerli olur
 
-# ---- şifreleme fonksiyonları ----
+# ---- ŞİFRELEME FONKSİYONLARI ----
 
 # kullanıcı şifresi ile db'deki enkript edilmiş şifreyi karşılaştırır
 def verify_password(plain_password, hashed_password):
@@ -20,3 +20,28 @@ def verify_password(plain_password, hashed_password):
 # düz metin halindeki şifreyi bcrypt ile enkript eder
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+# --- TOKENİZASYON FONKSİYONLARI ---
+
+# kullanıcı bilgileriyle imzalı bir jwt (token) oluşturur
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp": expire})
+    # token "secret key" ile imzalanır
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# gelen token'ın süresini ve geçerliliğini kontrol eder
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        return None  
+    except jwt.InvalidTokenError:
+        return None
