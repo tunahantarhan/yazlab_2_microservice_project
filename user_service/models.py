8ab8a7ac225babc054545636d791b21b08696a6a
tunahantarhan <tunahantarhan99@gmail.com>
2026-03-31 15:02:10 +0300

    feat(user): kullanıcı düzenleme altyapısı güncellemesi adına yeni UserUpdate şablonu eklendi.

--- user_service/models.py
+++ user_service/models.py
@@ -1,7 +1,13 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class User(BaseModel):
     id: int
     username: str
     email: str
     balance: float
+    
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[str] = None
+    balance: Optional[float] = None
